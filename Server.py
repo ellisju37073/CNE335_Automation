27039d34babdb031feff3e1344219d156e3571a2
ellisju37073 <67281488+ellisju37073@users.noreply.github.com>
2022-10-11 08:35:44 -0700

    Update

--- Server.py
+++ Server.py
@@ -1,3 +1,5 @@
+import os
+
 class Server:
     """ Server class for representing and manipulating servers. """
 
@@ -7,4 +9,5 @@ class Server:
 
     def ping(self):
         # TODO - Use os module to ping the server
-        return
+        result = os.system("ping -n 3 %s" % self.server_ip) #https://stackoverflow.com/questions/48468135/baccess-denied-option-c-requires-administrative-privileges
+        return result
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-# This is the template code for the CNA337 Final Project
+# Automate Ping
 # Zachary Rubin, zrubin@rtc.edu
 # CNA 337 Fall 2020
+from Server import Server
 
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Justin Ellis")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
+    my_server_ip = "34.209.241.29"
+    server=Server(my_server_ip)
     # TODO - Call Ping method and print the results
+    print(server.ping())
